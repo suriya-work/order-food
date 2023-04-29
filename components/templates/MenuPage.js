8222d395eebb98a319fbeff095b11f05376839e5
suriya babaei <suriyalifework60@gmail.com>
2023-04-29 08:55:30 -0700

    create menu and styles

---
+++ components/templates/MenuPage.js
@@ -0,0 +1,19 @@
+import Card from '../modules/Card';
+import styles from './MenuPage.module.css';
+const MenuPage = ({ data }) => {
+    return (
+
+        <div className={styles.container}>
+            <h2>Menu Food</h2>
+            <div className={styles.subContainer}>
+                {
+                    data.map((food) => (<Card key={food.id} { ...food } />))
+                }
+
+            </div>
+
+        </div>
+    );
+};
+
+export default MenuPage;
