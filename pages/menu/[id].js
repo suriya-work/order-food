0deff47a8e47ff4e56c0963dc1ab4078df1f6102
suriya babaei <suriyalifework60@gmail.com>
2023-05-01 22:43:27 -0700

    create route [id]

---
+++ pages/menu/[id].js
@@ -0,0 +1,48 @@
+import { useRouter } from "next/router";
+import DetailesPage from "@/components/templates/DetailesPage";
+
+const Detailes = ({ data }) => {
+    const router = useRouter();
+
+    if (router.isFallback) {
+        return <h2>Loading Page ...</h2>
+    }
+    return <DetailesPage { ...data } />
+};
+
+export default Detailes;
+
+//getStaticPaths
+
+export async function getStaticPaths() {
+    const res = await fetch("http://localhost:4000/data")
+    const json = await res.json()
+    const data = json.slice(0, 10)
+    const paths = data.map((food) => ({
+        params: { id: food.id.toString() },
+    }))
+
+    return {
+        paths,
+        fallback: true,
+    }
+}
+//getStaticProps
+
+export async function getStaticProps(context) {
+    const { params: { id } } = context;
+    const res = await fetch(`http://localhost:4000/data/${id}`);
+    const data = await res.json();
+
+    if (!data.id) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { data },
+        revalidate: 10,
+    }
+
+}
